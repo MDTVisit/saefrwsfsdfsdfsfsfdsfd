3855adb4fd7a4736db6bfba2ca2539546d09f95d
MDTVisit <112145096+MDTVisit@users.noreply.github.com>
2023-02-06 22:50:11 +0700

    dadadadad

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        double n = double.Parse(Console.ReadLine());
+
+        Console.WriteLine (F(n));
+
+
+    }
+    static double F(double X)
+    {
+
+                if (X>-1 &&X<=0)
+        return Math.Sqrt(1-(X*X));
+
+                if (X>0 &&X<1)
+        return -Math.Sqrt(1-(X*X));
+
+
+        if (X <= -1)
+        return F(X+2);
+
+        return F(X-2);
+
+
+    }
+
+} 
